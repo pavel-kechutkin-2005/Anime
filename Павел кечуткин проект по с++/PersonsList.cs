8bdfa230259a8b15b0f9997b9110e66bfa8a3ec0
mabramovsd <beavisabra@yandex.ru>
2021-02-05 18:38:18 +0400

    Список персонажей

---
+++ Павел кечуткин проект по с++/PersonsList.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Павел_кечуткин_проект_по_с__
+{
+    public partial class PersonsList : Form
+    {
+        public PersonsList()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
